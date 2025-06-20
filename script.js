bc2dd8ff8788aa734680916b5b91ca4c17ad8758
Gui <guillermogar@gmail.com>
2025-06-19 17:24:30 -0700

    description and fonts

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const apps = [
         {
+            name: '3 Tasks - Daily Productivity',
+            category: 'Productivity',
+            image: 'images/3 Tasks - Daily.png',
+            link: 'https://apps.apple.com/au/app/3-tasks-daily-productivity/id1553693943'
+        },
+        {
+            name: 'Drop: Relax Focus & Sleep',
+            category: 'Health & Fitness',
+            image: 'images/Drop meditation.png',
+            link: 'https://apps.apple.com/au/app/drop-relax-focus-sleep-well/id1462269642'
+        },
+        {
+            name: 'Only Gallery - Keeper',
+            category: 'Utilities',
+            image: 'images/Photo locker.png',
+            link: 'https://apps.apple.com/au/app/only-gallery-keeper/id1532670722'
+        },
+        {
+            name: 'Crypton - Crypto Tracker',
+            category: 'Finance',
+            image: 'images/Crypto.png',
+            link: 'https://apps.apple.com/au/app/crypton-crypto-tracker/id1143691286'
+        },
+        {
             name: 'Brainy: Imagine AI Art',
             category: 'Productivity',
             image: 'images/Brainy.png',
@@ -11,30 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
             category: 'Productivity',
             image: 'images/Chat AI.png',
             link: 'https://apps.apple.com/au/app/chat-with-fast-ai/id1662608035'
-        },
-        {
-            name: 'Drop: Relax Focus & Sleep',
-            category: 'Health & Fitness',
-            image: 'images/Drop meditation.png',
-            link: 'https://apps.apple.com/au/app/drop-relax-focus-sleep-well/id1462269642'
-        },
-        {
-            name: 'Crypton - Crypto Tracker',
-            category: 'Finance',
-            image: 'images/Crypto.png',
-            link: 'https://apps.apple.com/au/app/crypton-crypto-tracker/id1143691286'
-        },
-        {
-            name: '3 Tasks - Daily Productivity',
-            category: 'Productivity',
-            image: 'images/3 Tasks - Daily.png',
-            link: 'https://apps.apple.com/au/app/3-tasks-daily-productivity/id1553693943'
-        },
-        {
-            name: 'Only Gallery - Keeper',
-            category: 'Utilities',
-            image: 'images/Photo locker.png',
-            link: 'https://apps.apple.com/au/app/only-gallery-keeper/id1532670722'
         }
     ];
 
