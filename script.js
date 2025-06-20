0d3f028033770020ac35e21c9dcb641d42fc7b56
Gui <guillermogar@gmail.com>
2025-06-19 17:15:59 -0700

    links and more

--- script.js
+++ script.js
@@ -4,37 +4,37 @@ document.addEventListener('DOMContentLoaded', function() {
             name: 'Brainy: Imagine AI Art',
             category: 'Productivity',
             image: 'images/Brainy.png',
-            link: 'https://apps.apple.com/au/developer/saga-ltd/id1142866018'
+            link: 'https://apps.apple.com/au/app/brainy-imagine-ai-art/id1667947372'
         },
         {
             name: 'Chat with Fast AI',
             category: 'Productivity',
             image: 'images/Chat AI.png',
-            link: 'https://apps.apple.com/au/developer/saga-ltd/id1142866018'
+            link: 'https://apps.apple.com/au/app/chat-with-fast-ai/id1662608035'
         },
         {
             name: 'Drop: Relax Focus & Sleep',
             category: 'Health & Fitness',
             image: 'images/Drop meditation.png',
-            link: 'https://apps.apple.com/au/developer/saga-ltd/id1142866018'
+            link: 'https://apps.apple.com/au/app/drop-relax-focus-sleep-well/id1462269642'
         },
         {
             name: 'Crypton - Crypto Tracker',
             category: 'Finance',
             image: 'images/Crypto.png',
-            link: 'https://apps.apple.com/au/developer/saga-ltd/id1142866018'
+            link: 'https://apps.apple.com/au/app/crypton-crypto-tracker/id1143691286'
         },
         {
             name: '3 Tasks - Daily Productivity',
             category: 'Productivity',
             image: 'images/3 Tasks - Daily.png',
-            link: 'https://apps.apple.com/au/developer/saga-ltd/id1142866018'
+            link: 'https://apps.apple.com/au/app/3-tasks-daily-productivity/id1553693943'
         },
         {
             name: 'Only Gallery - Keeper',
             category: 'Utilities',
             image: 'images/Photo locker.png',
-            link: 'https://apps.apple.com/au/developer/saga-ltd/id1142866018'
+            link: 'https://apps.apple.com/au/app/only-gallery-keeper/id1532670722'
         }
     ];
 
